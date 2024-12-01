8202ee28963807361e6cfc2b936d91aa1a5e69ef
iahta <153889016+iahta@users.noreply.github.com>
2024-11-30 20:44:33 -0600

    characters counted

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def main():
     count_words = word_count(text)
     print(text)
     print(count_words)
+    print(character_count(text))
 
 def get_book_text(path):
     with open(path) as f:
@@ -12,5 +13,16 @@ def get_book_text(path):
 def word_count(text):
     words = text.split()
     return len(words)
+
+def character_count(text):
+    lowered_string = text.lower()
+    count = {}
+    for character in (lowered_string):
+        if character not in count:
+            count[character] = 1
+        elif character in count:
+                count[character] += 1
+    return count
+
 
 main()
